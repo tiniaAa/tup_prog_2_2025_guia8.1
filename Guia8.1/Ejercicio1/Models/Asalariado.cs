6b054bf919df1500e321586d8d658f0c0dff98df
tiniaAa <franciscovolovik@gmail.com>
2025-10-07 02:26:18 -0300

    act recien iniciada

---
+++ Guia8.1/Ejercicio1/Models/Asalariado.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Models
+{
+    public class Asalariado : Empleado, IExportable
+    {
+        public double Basico { get; private set; }
+        public double AportesPrevisionales { get; private set; }
+        public Asalariado(int dni, string nombre, double basico, double aporte ):base(dni,nombre) 
+        {
+            this.Basico = basico;
+            this.AportesPrevisionales= aporte;
+
+        }
+        public override double CalcularImportarAPagar()
+        {
+            double importe = Basico - AportesPrevisionales;
+
+            return importe;
+        }
+
+        public string[] Exportar()
+        {
+            List<Asalariado> exporte = new List<Asalariado>();
+            
+            
+            return;
+        }
+        public override string ToString()
+        {
+        }
+        public override string[] GenerarRecibo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Guia8.1/Ejercicio1/Models/Empleado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Models
+{
+    public abstract class Empleado : IComparable
+    {
+        public int DNI { get; private set; }
+        public string Nombre { get; private set; }
+
+        public Empleado(int dni, string nombre ) 
+        {
+            this.DNI = dni;
+            this.Nombre = nombre;
+        }
+
+        abstract public double CalcularImportarAPagar();
+        abstract public string[] GenerarRecibo();
+        abstract public string ToString();
+        public int CompareTo(object? obj ) 
+        {
+            Empleado empleado = obj as Empleado;
+            if ( empleado != null)
+            {
+                return DNI.CompareTo( empleado.DNI );
+            }
+            return -1;
+        }
+    }
+}
